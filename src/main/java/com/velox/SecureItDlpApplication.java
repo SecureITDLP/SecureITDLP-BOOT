4df4cff15f81510f885e23a4f776b2cfe2cb9340
velox.dev.prathamesh@gmail.com <wilypratham.sits.entc@gmail.com>
2026-05-19 13:13:01 +0530

    Initial SecureITDLP enterprise commit

---
+++ src/main/java/com/velox/SecureItDlpApplication.java
@@ -0,0 +1,13 @@
+package com.velox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureItDlpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureItDlpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/velox/SecureItDlpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.velox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureItDlpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
